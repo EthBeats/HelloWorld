6c4410a68d4543d8cd14ef88fd66f32290697d5e
EthBeats <ethbeats3@gmail.com>
2023-10-14 10:46:58 -0400

    Updated Text

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -19,7 +19,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::Font (48.0f));
     g.setColour (juce::Colours::red);
-    g.drawText ("Hello C++!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello JUCE::C++!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
